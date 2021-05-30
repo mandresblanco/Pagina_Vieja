9ae2e6deac977f0cd43f92e500021b34c7c0e287
mandresblanco <mandresblanco@outlook.com>
2021-05-29 19:24:50 -0500

    first commit

---
+++ js/bgParallax.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+  $(window).on('scroll', function () {
+
+    var contacto = $('#contacto');
+
+    if ($(window).scrollTop() >= contacto.offset().top - contacto.outerHeight()) {
+
+      var posicion = -($(window).scrollTop() - contacto.offset().top)*0.2;
+
+      contacto.css('background-position', '0 '+posicion+'px');
+
+    }
+
+
+  })
+
+});
